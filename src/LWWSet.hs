c3158736c459d40c8ffae9ca6dd840f1c363699a
Ilya Pavlenkov <j@rtur.me>
2018-05-16 21:05:48 +0200

    Added Haddock documentation

--- src/LWWSet.hs
+++ src/LWWSet.hs
@@ -30,42 +30,42 @@ tsSetLookup (TimeStampedSet m) x = M.lookup x m >>= \ts -> return (x, ts)
 tsSetInsert :: (Ord a) => TimeStampedSet a -> a -> TimeStamp -> TimeStampedSet a
 tsSetInsert (TimeStampedSet m) x ts = TimeStampedSet $ M.insert x ts m
 
--- The LWW-Element-Set datastructure
+-- | The LWW-Element-Set datastructure
 data LWWSet a = LWWSet { lwwAdded :: TimeStampedSet a 
                         ,lwwRemoved :: TimeStampedSet a }
     deriving (Show)
 
--- Monoid instance is the essence of it being mergeable
+-- | 'Monoid' instance is the essence of it being mergeable
 instance Ord a => Monoid (LWWSet a) where 
     mempty = LWWSet mempty mempty
     mappend (LWWSet leftAdded leftRem) (LWWSet rightAdded rightRem) = 
         -- Basically we just merge add set and remove set using the previously defined monoid instance
         LWWSet (leftAdded `mappend` rightAdded) (leftRem `mappend` rightRem)
 
--- We consider two LWW sets equal if their reductions to normal sets are equal
+-- | We consider two 'LWWSet's equal if their reductions to normal 'S.Set' are equal
 instance (Eq a, Ord a) => Eq (LWWSet a) where
     s1 == s2 = (toSet s1) == (toSet s2)
 
--- Reduce LWW set to a normal set whic contains only non-deleted elements
+-- | Reduce 'LWWSet' to a normal 'S.Set' which contains only non-deleted elements
 toSet :: (Ord a) => LWWSet a -> S.Set a
 toSet s@(LWWSet (TimeStampedSet addMap) _) = 
     M.foldMapWithKey present addMap
     where 
         present x _ = if query s x then S.singleton x else S.empty
 
--- Here we use the fact that LWWSet is a monoid
+-- | Here we use the fact that 'LWWSet' is a 'Monoid'
 merge :: (Ord a) => LWWSet a -> LWWSet a -> LWWSet a
 merge = mappend
 
--- Construct an empty set, also from monoid
+-- | Construct an empty 'LWWSet', also from 'Monoid' instance
 empty :: (Ord a) => LWWSet a
 empty = mempty
 
--- Construct an LWW set with a single element 
+-- | Construct an 'LWWSet' with a single element 
 unit :: (Ord a) => a -> TimeStamp -> LWWSet a
 unit x ts = insert mempty x ts
 
--- Check if the element is in the LWW set, which means it was added the latest add timestamp
+-- | Check if the element is in the 'LWWSet', which means it was added the latest add timestamp
 -- is greater that the last removal if any. Here we are biased to removal.
 query :: (Ord a) => LWWSet a -> a -> Bool
 query (LWWSet addSet remSet) x = maybe False checkRemoval maybeAdded
@@ -76,10 +76,10 @@ query (LWWSet addSet remSet) x = maybe False checkRemoval maybeAdded
                 Just (_, removedTs) -> removedTs < addedTs -- Biased to removal
                 Nothing -> True
 
--- Add new element to the LWW set                
+-- | Add new element to the 'LWWSet'                
 insert :: (Ord a) => LWWSet a -> a -> TimeStamp -> LWWSet a
 insert (LWWSet add rem) x ts = LWWSet (tsSetInsert add x ts) rem
 
--- Remove an element from the LWW set
+-- | Remove an element from the 'LWWSet'
 remove :: (Ord a) => LWWSet a -> a -> TimeStamp -> LWWSet a
 remove (LWWSet add rem) x ts = LWWSet add (tsSetInsert rem x ts)
